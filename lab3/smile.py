d19eab52add094ee912d3be03a1b3f1aee4657d9
khomiakov <khomiakov.io@phystech.edu>
2020-09-22 16:44:41 +0300

    smile.py

--- lab3/smile.py
+++ lab3/smile.py
@@ -1,4 +1,13 @@
 import pygame
-import pygame.drow as d
+import pygame.draw as d
+
 pygame.init()
-screen = pygame.display.set_mode((300, 200))
+screen = pygame.display.set_mode((400, 400))
+d.rect(screen, (100, 0, 100), (100, 100, 100, 200))
+pygame.display.update()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+clock = pygame.time.Clock()
+clock.tick(30)
